c21b2b9ae8a90808609051d86b94033cc9ae560e
EdricY <yu.edric@gmail.com>
2016-01-28 19:47:48 -0600

    pixLab part 5

---
+++ pixLab Part 5/classes/test.java
@@ -0,0 +1,21 @@
+package pixLab.classes;
+
+
+/**
+ * Write a description of class test here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class test
+{
+    public void sampleMethod()
+    {
+        //DigitalPicture a = new DigitalPicture(); //no
+        DigitalPicture b = new SimplePicture();    //yes
+        DigitalPicture c = new Picture();          //yes
+        SimplePicture d = new Picture();           //yes
+        //Picture e = new SimplePicture();         //no
+
+    }
+}
