d3d41658c5cd3c4f0f0c83830ecbe840b7e7b1be
EdricY <yu.edric@gmail.com>
2016-01-28 19:34:34 -0600

    Magpie Part 3

---
+++ Magpie Part 3/MagpieRunner3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * A simple class to run the Magpie class.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class MagpieRunner3
+{
+
+	/**
+	 * Create a Magpie, give it user input, and print its replies.
+	 */
+	public static void main(String[] args)
+	{
+		Magpie3 maggie = new Magpie3();
+		for(int i = 0; i <= 50; i++)
+        {
+        System.out.print("\n");
+        }
+		System.out.println (maggie.getGreeting());
+		Scanner in = new Scanner (System.in);
+		String statement = in.nextLine();
+		
+		while (!statement.toLowerCase().equals("bye"))
+		{
+			System.out.println (maggie.getResponse(statement));
+			statement = in.nextLine();
+		}
+	}
+
+}
---
+++ Magpie Part 3/StringExplorer.java
@@ -0,0 +1,29 @@
+/**
+ * A program to allow students to try out different 
+ * String methods. 
+ * @author Laurie White
+ * @version April 2012
+ */
+public class StringExplorer
+{
+
+	public static void main(String[] args)
+	{
+		String sample = "The quick brown fox jumped over the lazy dog.";
+		
+		//  Demonstrate the indexOf method.
+		int position = sample.indexOf("quick", 5);
+		System.out.println ("sample.indexOf(\"quick\") = " + position);
+		int notFoundPsn = sample.indexOf("slow");
+		System.out.println("sample.indexOf(\"slow\") = " + notFoundPsn);
+		//  Demonstrate the toLowerCase method.
+		String lowerCase = sample.toLowerCase();
+		System.out.println ("sample.toLowerCase() = " + lowerCase);
+		System.out.println ("After toLowerCase(), sample = " + sample);
+		
+		//  Try other methods here:
+		
+
+	}
+
+}
